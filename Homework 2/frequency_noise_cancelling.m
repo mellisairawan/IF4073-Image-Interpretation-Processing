d6623a33059370ab834155838318450ee54dc299
kinantanbagaspati <kinantanyolo@gmail.com>
2022-10-06 23:56:50 +0700

    done

--- Homework 2/frequency_noise_cancelling.m
+++ Homework 2/frequency_noise_cancelling.m
@@ -1,4 +1,6 @@
 function result_img = frequency_noise_cancelling(img, rows, cols)
+    % INPUT image matrix, array absis dan array ordinat semua titik yang ingin di 0 kan
+    % OUTPUT matrix hasil pengembalian ke spatial
     frequency_img = fft2(double(img));
     frequency_img = fftshift(frequency_img);
     
@@ -8,6 +10,8 @@ function result_img = frequency_noise_cancelling(img, rows, cols)
     debug = log(abs(frequency_img) + 1);
     figure; imagesc(uint8(10 * debug));
     
+    % pengnolan semua titik dengan absis dari array rows dan ordinat dari
+    % array cols
     for i=1:len
         for k=1:d
             frequency_img(rows(i), cols(i), k) = 0;
